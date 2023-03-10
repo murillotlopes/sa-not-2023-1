887cfc724ea045bd2b45f2d34d774f4e6f1294a5
murillotlopes <murillo.tlopes@hotmail.com>
2023-03-10 23:11:43 +0000

    correção do virus fake

--- xss/public/javascripts/virus.js
+++ xss/public/javascripts/virus.js
@@ -1,12 +1,16 @@
 let normal = false
-setInterval(() => {
-  let body = document.body
-  if (normal) {
-    body.style.color = 'black'
-    body.style.backgroundColor = 'white'
-  } else {
-    body.style.color = 'white'
-    body.style.backgroundColor = 'black'
-  }
-  normal = !normal
-}, 1000)
+function start() {
+  setInterval(() => {
+    let body = document.body
+    if (normal) {
+      body.style.color = 'black'
+      body.style.backgroundColor = 'white'
+    } else {
+      body.style.color = 'white'
+      body.style.backgroundColor = 'black'
+    }
+    normal = !normal
+  }, 1000)
+}
+
+start()
